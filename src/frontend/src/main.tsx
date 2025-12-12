52547d843050de32659431a1661917a57097e990
dataflowslab <mihai@dataflows.ro>
2025-12-12 02:01:13 +0200

    Fix: Adaugat ModalsProvider pentru functionalitate modals (stergere semnatura)

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
+import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
@@ -17,12 +18,14 @@ import './styles/mobile.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MantineProvider defaultColorScheme="light">
-      <Notifications position="top-right" />
-      <BrowserRouter basename="/web">
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </BrowserRouter>
+      <ModalsProvider>
+        <Notifications position="top-right" />
+        <BrowserRouter basename="/web">
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </BrowserRouter>
+      </ModalsProvider>
     </MantineProvider>
   </React.StrictMode>
 );
