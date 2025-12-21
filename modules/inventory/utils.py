e3add8bab99d4df18f956cf85874863520652419
dataflowslab <mihai@dataflows.ro>
2025-12-21 19:24:03 +0200

    refactor: Created models.py and utils.py to organize inventory module code

---
+++ modules/inventory/utils.py
@@ -0,0 +1,27 @@
+"""
+Utility functions for Inventory module
+"""
+from bson import ObjectId
+
+
+def serialize_doc(doc):
+    """Convert MongoDB document to JSON-serializable format"""
+    if doc is None:
+        return None
+    if isinstance(doc, list):
+        return [serialize_doc(item) for item in doc]
+    if isinstance(doc, dict):
+        result = {}
+        for key, value in doc.items():
+            if key == '_id' or key.endswith('_id'):
+                result[key] = str(value) if value else None
+            elif isinstance(value, ObjectId):
+                result[key] = str(value)
+            elif isinstance(value, dict):
+                result[key] = serialize_doc(value)
+            elif isinstance(value, list):
+                result[key] = [serialize_doc(item) if isinstance(item, dict) else item for item in value]
+            else:
+                result[key] = value
+        return result
+    return doc
