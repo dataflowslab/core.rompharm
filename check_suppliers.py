b5fc775a4e32dce6bf3a8d2da60c1177cf818861
dataflowslab <mihai@dataflows.ro>
2026-01-25 23:26:46 +0200

    chore: Move test/fix/check scripts to utils and update gitignore

--- check_suppliers.py
+++
@@ -1,8 +0,0 @@
-"""Check if suppliers routes exist"""
-from modules.inventory.routes import router
-
-suppliers_routes = [r for r in router.routes if 'suppliers' in r.path]
-print(f'Found {len(suppliers_routes)} supplier routes:')
-for r in suppliers_routes:
-    method = list(r.methods)[0] if r.methods else 'GET'
-    print(f'  {method} {r.path}')
